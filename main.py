23c36b442011eba89db7bc61cb45e3bc536cf675
Jannis Pohl <122696596+jannis-chr@users.noreply.github.com>
2024-04-12 15:10:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import json
 import my_functions as mf
-
+#Erstellung des Probanden
 subject = mf.build_person(
     input("Geben Sie den Vornamen des Probande:in ein:"),
     input("Geben Sie den Nachnamen des Probande:in ein:"),
     input("Geben sie das Geschlecht 'male' oder 'female' an:"),
     int(input("Geben Sie das Alter des Probande:in ein:"))
    )
+#Erstellung des Supervisors
 supervisor = mf.build_person(
     input("Geben Sie den Vornamen des Supervisors ein:"),
     input("Geben Sie den Nachnamen des Supervisors ein:"),
     input("Geben sie das Geschlecht des Supervisors 'male' oder 'female' an:"),
     int(input("Geben Sie das Alter des Supervisors ein:"))
 )
-            
+#Input für Datum und Experimentname             
 experiment = mf.build_experiment(
     input("Geben Sie den Experimentnamen ein:"),
     input("Geben Sie das Datum ein:"),
