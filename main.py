4b2e0c844ef53e800057885c7abcdffecad3727b
kf7091 <kf7091@mci4me.at>
2024-04-24 22:31:08 +0200

    class Person was got children edit main.py to accomodate children

--- main.py
+++ main.py
@@ -2,23 +2,21 @@ from datetime import datetime
 import myclasses as mc
 
 #create subject
-subject = mc.Person(
+subject = mc.Subject(
     input("Geben Sie den Vornamen des Probande:in ein:"),
     input("Geben Sie den Nachnamen des Probande:in ein:"),
     input("Geben sie das Geschlecht 'male' oder 'female' an:"),
-    int(input("Geben Sie das Alter des Probande:in ein:"))
+    datetime.strptime(input("Geben Sie das Geburtsdatum des Probanden ein:"), '%d.%m.%Y')
    )
 #create supervisor
-supervisor = mc.Person(
+supervisor = mc.Supervisor(
     input("Geben Sie den Vornamen des Supervisors ein:"),
     input("Geben Sie den Nachnamen des Supervisors ein:"),
-    input("Geben sie das Geschlecht des Supervisors 'male' oder 'female' an:"),
-    int(input("Geben Sie das Alter des Supervisors ein:"))
 )
 #create experiment            
 experiment = mc.Experiment(
     input("Geben Sie den Experimentnamen ein:"),
-    datetime.strptime(input("Geben Sie das Datum ein:"), '%d.%m.%y'),
+    datetime.strptime(input("Geben Sie das Datum ein:"), '%d.%m.%Y'),
     supervisor.__dict__,
     subject.__dict__
 )
--- myclasses.py
+++ myclasses.py
@@ -3,26 +3,9 @@ import os
 from datetime import datetime
 
 class Person():  
-    def __init__(self, first_name : str, last_name : str, sex : str, age : int):
+    def __init__(self, first_name : str, last_name : str):
         self.first_name : str = first_name
         self.last_name : str = last_name
-        self.sex : str = sex
-        self.age : int = age
-        self.est_max_hr : int = self.estimate_max_hr()
-    
-    #estimating the maximum heart rate with standard formulas
-    def estimate_max_hr(self) -> int:
-        """
-        See https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4124545/ for different formulas
-        """
-        if self.sex == "male":
-            max_hr_bpm =  223 - 0.9 * self.age
-        elif self.sex == "female":
-            max_hr_bpm = 226 - 1.0 *  self.age
-        else:
-            # der input() öffnet ein Eingabefenster für den Nutzer und speichert die Eingabe
-            max_hr_bpm  = input("Enter maximum heart rate:")
-        return int(max_hr_bpm)
     
     def save(self):
         '''
@@ -38,11 +21,43 @@ class Person():
             json.dump(self.__dict__, outfile)
             print(f"JSON-Datei mit folgenden Inhalt wurde wurde in {os.sep.join(["output", subfolder])} erstellt:\n", self.__dict__)
 
+class Supervisor(Person):
+    def __init__(self, first_name : str, last_name : str):
+        super().__init__(first_name, last_name)
+
+class Subject(Person):
+    def __init__(self, first_name : str, last_name : str, sex : str, date_of_birth : datetime):
+        super().__init__(first_name, last_name)
+        self.sex : str = sex
+        self.__date_of_birth : str = date_of_birth.strftime("%Y.%m.%d")
+        self.est_max_hr : int = self.estimate_max_hr()
+    
+    #estimating the maximum heart rate with standard formulas
+    def estimate_max_hr(self) -> int:
+        """
+        See https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4124545/ for different formulas
+        """
+        if self.sex == "male":
+            max_hr_bpm =  223 - 0.9 * self.get_age()
+        elif self.sex == "female":
+            max_hr_bpm = 226 - 1.0 *  self.get_age()
+        else:
+            # der input() öffnet ein Eingabefenster für den Nutzer und speichert die Eingabe
+            max_hr_bpm  = input("Enter maximum heart rate:")
+        return int(max_hr_bpm)
+    
+    def get_age(self) -> int:
+        # idea from https://miguendes.me/how-to-use-datetimetimedelta-in-python-with-examples#heading-how-to-convert-a-timedelta-to-years
+        
+        age_delta = datetime.now() - datetime.strptime(self.__date_of_birth, '%Y.%m.%d')
+        return int(age_delta.days / 365.25)
+                 
+
 
 class Experiment():
     def __init__(self, experiment_name : str, date : datetime, supervisor : dict, subject : dict):
         self.experiment_name : str = experiment_name
-        self.date : str = date.strftime("%Y.%m.%d")
+        self.date : str = date
         self.supervisor : dict = supervisor
         self.subject : dict = subject
 
@@ -50,12 +65,17 @@ class Experiment():
         '''
         saves the experiment object as a json file in the output/experiments folder
         '''
+
         #creating the subfolder for the experiments if it does not exist
         subfolder : str = "experiments"
         if not os.path.exists("output"): os.makedirs("output")
         if not os.path.exists(os.sep.join(["output", subfolder])): os.makedirs(os.sep.join(["output", subfolder]))
 
+        self.date = self.date.strftime("%Y.%m.%d") #converting the date to string for json
+
         #writing the json file
         with open(os.sep.join(["output", subfolder, f"{self.date}_{self.experiment_name}.json"]), "w") as outfile: 
             json.dump(self.__dict__, outfile)
             print(f"JSON-Datei für diese Experiment mit folgenden Inhalt wurde wurde in {os.sep.join(["output", subfolder])} erstellt:\n", self.__dict__)
+        
+        self.date = datetime.strptime(self.date, '%Y.%m.%d') #reverting the date back to datetime object
